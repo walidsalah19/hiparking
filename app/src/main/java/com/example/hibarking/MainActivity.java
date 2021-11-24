85b8e8ec7e8b8ac6b582e791b2b408f8ea49155d
walid salah <walidsalah9585@gmail.om>
2021-11-24 03:34:03 +0200

    update

--- app/src/main/java/com/example/hibarking/MainActivity.java
+++ app/src/main/java/com/example/hibarking/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //create
         toolpar_intialize();
         firebase_tool_intialize();
         check_user_acess();
